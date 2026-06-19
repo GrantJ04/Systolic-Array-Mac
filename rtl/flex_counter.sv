f6d950d1363d54148f96e6bc1d1559d31d66a296
Grant Johnson <grantjohnson0408@gmail.com>
2026-06-19 16:44:32 -0500

    SRAM controller implemented + fixes and adjustments to flex_counter

--- rtl/flex_counter.sv
+++ rtl/flex_counter.sv
@@ -6,17 +6,19 @@ module flex_counter #(
     input logic clk,
     input logic n_rst,
     input logic en,
-    output logic rollover_flag
+    output logic rollover_flag,
+    output logic [$clog2(DEPTH)-1:0] count
 );
-    logic [$clog2(DEPTH)-1:0] count;
-    always_ff @(posedge clk) begin
+    localparam CNT_WIDTH = $clog2(DEPTH);
+
+    always_ff @(posedge clk, negedge n_rst) begin
         if(!n_rst) begin
             count <= '0;
             rollover_flag <= 1'b0;
         end else begin
-            rollover_flag <= en && (count == DEPTH-1);
+            rollover_flag <= en && (count == CNT_WIDTH'(DEPTH-1));
             if (en) begin
-                if(count == DEPTH-1) begin
+                if(count == CNT_WIDTH'(DEPTH-1)) begin
                     count <= '0;
                 end else begin
                     count <= count + 1;
--- rtl/sram_controller.sv
+++ rtl/sram_controller.sv
@@ -6,15 +6,54 @@ typedef enum logic[2:0]{
     OUTPUT_READY
 } state_t;
 
-module sram_controller #()(
-    input clk,
-    input n_rst,
-    
+module sram_controller #(
+    parameter DATA_WIDTH = 8,
+    parameter DEPTH = 16
+)(
+    input logic clk,
+    input logic n_rst,
+    input logic start,
+    input logic [DATA_WIDTH-1:0] weight_in,
+    output logic we,
+    output logic [$clog2(DEPTH)-1:0] addr,
+    output logic [DATA_WIDTH-1:0] din
 );
     state_t state, next_state;
+    logic weights_loaded, drain_fin;
+
+    flex_counter #() w_ctr(
+        .clk(clk),
+        .n_rst(n_rst),
+        .en(state == LOAD_WEIGHTS || state == STREAM_INPUT),
+        .rollover_flag(weights_loaded),
+        .count(addr)
+    );
+
+    flex_counter #(.DEPTH(12)) drain(
+        .clk(clk),
+        .n_rst(n_rst),
+        .en(state == STREAM_INPUT),
+        .rollover_flag(drain_fin),
+        .count()
+    );
 
     always_comb begin
-
+        next_state = state;
+        case(state)
+        IDLE: begin
+            if(start) next_state = LOAD_WEIGHTS;
+        end
+        LOAD_WEIGHTS: begin
+            if(weights_loaded) next_state = STREAM_INPUT;
+        end
+        STREAM_INPUT: begin
+            if(drain_fin) next_state = OUTPUT_READY;
+        end
+        OUTPUT_READY: begin
+            next_state = IDLE;
+        end
+        default: next_state = IDLE;
+        endcase
     end
 
     always_ff @(posedge clk, negedge n_rst) begin
@@ -24,5 +63,8 @@ module sram_controller #()(
             state <= next_state;
         end
     end
+
+    assign we = (state == LOAD_WEIGHTS);
+    assign din = weight_in;
 
 endmodule
