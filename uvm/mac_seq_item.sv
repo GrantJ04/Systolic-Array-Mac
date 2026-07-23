981d211ddf4a075633a44c23ad1c4dd6986bbddc
Grant <grantjohnson0408@gmail.com>
2026-07-23 14:20:45 -0500

    Enhance mac_seq_item with start field and constraints
    Added 'start' field and updated constraints for mac_seq_item.

--- uvm/mac_seq_item.sv
+++ uvm/mac_seq_item.sv
@@ -1,12 +1,14 @@
 class mac_seq_item extends uvm_sequence_item;
     rand logic [7:0] w_in;
     rand logic [7:0] x_in;
+    rand logic start;
     `uvm_object_utils(mac_seq_item)
     function new(string name = "mac_seq_item");
         super.new(name);
     endfunction
     constraint c_array {
-        foreach(w_in[i]) w_in[i] inside {[1:50]};
-        foreach(x_in[i]) x_in[i] inside {[1:50]};
+       w_in inside {[1:50]};
+       x_in inside {[1:50]};
+       start dist {0 :=95, 1:= 5};
     }
 endclass
