cac2662352c351ba2cb5038b528a4512dc970655
Grant <grantjohnson0408@gmail.com>
2026-07-24 09:35:00 -0500

    Add mac_test class for UVM testing

---
+++ uvm/mac_test.sv
@@ -0,0 +1,18 @@
+class mac_test extends uvm_test;
+  `uvm_component_utils(mac_test);
+
+  mac_env env;
+  mac_sequence seq;
+  virtual function void build_phase(uvm_build_phase phase);
+    super.build_phase(phase);
+    env = mac_env::type_id::create("env", this);
+  endfunction
+
+  virtual task run_phase(uvm_run_phase phase);
+    seq = mac_sequence::type_id::create("seq");
+    phase.raise_objection(this);
+    seq.start(env.agent.sequencer);
+    phase.drop_objection(this);
+  endtask
+endclass
+    
