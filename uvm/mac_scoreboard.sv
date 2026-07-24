6ed5531a4435dce3a79d29cc495b8d28ada0fb01
Grant <grantjohnson0408@gmail.com>
2026-07-24 14:15:47 -0500

    Enhance write function for multi-dimensional validation
    Refactor scoreboard validation to check multiple outputs.

--- uvm/mac_scoreboard.sv
+++ uvm/mac_scoreboard.sv
@@ -15,18 +15,28 @@ class mac_scoreboard extends uvm_scoreboard;
   
   logic [7:0] x_history[$];
   logic [7:0] w_history[$];
-  
+
   virtual function void write(mac_seq_item tr);
     x_history.push_back(tr.x_in);
     w_history.push_back(tr.w_in);
     if(x_history.size() > 4) x_history.pop_front();
     if(w_history.size() > 4) w_history.pop_front();
-    if(tr.valid_out) begin
-      if(tr.y_i == '0) begin
-        uvm_error("SCOREBOARD", "Output y_i should not be zero when valid_out is high");
+    for(int i = 0; i < 4; i++) begin
+      for(int j = 0; j < 4; j++) begin
+        if(tr.valid_out[i][j]) begin
+          logic [17:0] expected = 0;
+          for(int k = 0; k <= j && k < x_history.size(); k++) begin
+            expected += tr.w_in * x_history[k];
+          end
+          if(tr.y_i[i][j] != expected) begin
+            uvm_error("SCOREBOARD", $sformatf(
+              "Cell[%0d][%0d]: Expected %0d, got %0d",
+              i, j, expected, tr.y_i[i][j]
+            ));
+          end
         end
+      end
     end
-    
   endfunction
 endclass
   
