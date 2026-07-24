a5feb80698741424b297aa70df98cb04dcbf101d
Grant <grantjohnson0408@gmail.com>
2026-07-24 14:10:15 -0500

    Add history tracking for x_in and w_in

--- uvm/mac_scoreboard.sv
+++ uvm/mac_scoreboard.sv
@@ -12,8 +12,15 @@ class mac_scoreboard extends uvm_scoreboard;
     super.build_phase(phase);
     in = new("in", this);
   endfunction
-
+  
+  logic [7:0] x_history[$];
+  logic [7:0] w_history[$];
+  
   virtual function void write(mac_seq_item tr);
+    x_history.push_back(tr.x_in);
+    w_history.push_back(tr.w_in);
+    if(x_history.size() > 4) x_history.pop_front();
+    if(w_history.size() > 4) w_history.pop_front();
     if(tr.valid_out) begin
       if(tr.y_i == '0) begin
         uvm_error("SCOREBOARD", "Output y_i should not be zero when valid_out is high");
