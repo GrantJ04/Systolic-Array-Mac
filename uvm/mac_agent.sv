71ab94634fa32c905d5a64814af17c6c52366e8a
Grant <grantjohnson0408@gmail.com>
2026-07-24 08:19:56 -0500

    Add mac_agent class for MAC protocol handling

---
+++ uvm/mac_agent.sv
@@ -0,0 +1,24 @@
+class mac_agent extends uvm_agent;
+  `uvm_component_utis(mac_agent);
+  
+  uvm_sequencer #(mac_seq_item) sequencer;
+  mac_driver driver;
+  mac_monitor monitor;
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  virtual function void build_phase(uvm_build_phase phase);
+    sequencer = uvm_sequencer#(mac_seq_item)::type_id::create("sequencer", this);
+    driver = mac_driver::type_id::create("driver", this);
+    monitor = mac_monitor::type_id::create("monitor", this);
+  endfunction
+
+  virtual function void connect_phase(uvm_connect_phase phase);
+    super.connect_phase(phase);
+    driver.seq_item_port.connect(sequencer.seq_item_export);     
+  endfunction
+  
+endclass
+  
