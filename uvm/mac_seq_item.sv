2935c2946f2c8e5a313594d7a2a65a0d0c49ce79
Grant <grantjohnson0408@gmail.com>
2026-07-24 07:48:48 -0500

    Add valid_out and y_i fields to mac_seq_item

--- uvm/mac_seq_item.sv
+++ uvm/mac_seq_item.sv
@@ -2,6 +2,8 @@ class mac_seq_item extends uvm_sequence_item;
     rand logic [7:0] w_in;
     rand logic [7:0] x_in;
     rand logic start;
+    logic [3:0][3:0] valid_out;
+    logic [3:0][3:0][17:0] y_i;
     `uvm_object_utils(mac_seq_item)
     function new(string name = "mac_seq_item");
         super.new(name);
